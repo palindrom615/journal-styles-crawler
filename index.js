1a361d646cc5f14dd5325ca3897c0d8d91ba0a03
Jang Whemoon <palindrom615@gmail.com>
2020-05-24 17:54:26 +0900

    Add screenshot

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 const puppeteer = require("puppeteer");
 // const devices = require("puppeteer/DeviceDescriptors");
 const device = puppeteer.devices["Galaxy Note 3"];
-
 (async () => {
   const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.emulate(device);
-  await page.goto("https://m.khan.co.kr/view.html?art_id=202005240837001");
-  // await page.screenshot({ path: "example.png", fullPage: true });
-  // const nodes = await page.$$("*");
 
-  // const nodeArr = Array.from(nodes);
+  const urls = ["https://m.khan.co.kr/view.html?art_id=202005240837001"];
 
-  const texts = await page.$x("//text()[not(parent::script|parent::style)]");
-  console.log(texts[0])
-  const texts_innerText = await Promise.all(
-    texts.map((e) =>
+  const getInfo = async (url) => {
+    const { hostname } = new URL(url);
+    console.log(hostname)
+    const page = await browser.newPage();
+    await page.emulate(device);
+    await page.goto(url);
+    const scsPromise = page.screenshot({ path: hostname+'.png', fullPage: true });
+    // const nodes = await page.$$("*");
+
+    // const nodeArr = Array.from(nodes);
+
+    const texts = await page.$x(
+      "//text()[not(parent::script|parent::style)]"
+    );
+    console.log(texts);
+
+    const textsInnerTextPromise = texts.map((e) =>
       e.evaluate((e) => {
         style = getComputedStyle(e.parentNode);
         return {
@@ -25,7 +32,11 @@ const device = puppeteer.devices["Galaxy Note 3"];
           fontFamily: style.getPropertyValue("font-family"),
         };
       })
-    )
-  );
+    );
+    return Promise.all([scsPromise, ...textsInnerTextPromise]);
+  };
+
+  await Promise.all(urls.map(getInfo));
+
   await browser.close();
 })();
