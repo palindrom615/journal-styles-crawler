22c999f3f433083aad9970b07dd0b9f76b7dd5c2
Jang Whemoon <palindrom615@gmail.com>
2020-05-24 19:52:16 +0900

    Update journals

--- index.js
+++ index.js
@@ -4,17 +4,26 @@ const fs = require("fs").promises;
 const device = puppeteer.devices["Galaxy Note 3"];
 
 (async () => {
-  const urls = (await fs.readFile("./urls", "utf8")).trim().split("\n");
+  const journals = (await fs.readFile("./journals", "utf8"))
+    .trim()
+    .split("\n")
+    .map((string) => string.split(","))
+    .filter((arr) => arr.length === 2 && arr[1] !== "");
   const browser = await puppeteer.launch();
 
-  const getInfo = async (url) => {
+  const getInfo = async ([journal, url]) => {
     const page = await browser.newPage();
     await page.emulate(device);
-    await page.goto(url);
+    try {
+      await page.goto(url);
+    } catch (e) {
+      console.log(e, journal, url);
+      return;
+    }
 
     const { hostname } = new URL(url);
     const scsPromise = page.screenshot({
-      path: hostname + ".png",
+      path: journal + ".png",
       fullPage: true,
     });
 
@@ -35,14 +44,20 @@ const device = puppeteer.devices["Galaxy Note 3"];
     return Promise.all([scsPromise, ...textStylePromises]);
   };
 
-  const results = await Promise.all(urls.map(getInfo));
-  const resultsJson = results.map(([_, ...textStyle], urlIdx) => {
-    // find second paragraph over 30 words.
-    const mainContentFirst = textStyle.filter(
-      (t) => t.txt.trim().split(" ").length > 30
-    );
-    return { site: urls[urlIdx], ...mainContentFirst[1] };
-  });
+  const results = await Promise.all(journals.map(getInfo));
+  const resultsJson = results
+    .filter((result) => typeof result !== "undefined")
+    .map(([_, ...textStyle], journalIdx) => {
+      // find second paragraph over 30 words.
+      const mainContentFirst = textStyle.filter(
+        (t) => t.txt.trim().split(" ").length > 30
+      );
+      return {
+        journal: journals[journalIdx][0],
+        site: journals[journalIdx][1],
+        ...mainContentFirst[1],
+      };
+    });
   await browser.close();
 
   await fs.writeFile("result.json", JSON.stringify(resultsJson));
