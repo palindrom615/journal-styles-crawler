3eb5bdbc1e38b24f8a9f55c7c54fd46a5e5c0d0b
Jang Whemoon <palindrom615@gmail.com>
2020-05-24 18:43:18 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
 const puppeteer = require("puppeteer");
-const fs = require("fs");
-// const devices = require("puppeteer/DeviceDescriptors");
+const fs = require("fs").promises;
+
 const device = puppeteer.devices["Galaxy Note 3"];
+
 (async () => {
+  const urls = (await fs.readFile("./urls", "utf8")).trim().split("\n");
   const browser = await puppeteer.launch();
 
-  const urls = ["https://m.khan.co.kr/view.html?art_id=202005240837001"];
-
   const getInfo = async (url) => {
-    const { hostname } = new URL(url);
     const page = await browser.newPage();
     await page.emulate(device);
     await page.goto(url);
+
+    const { hostname } = new URL(url);
     const scsPromise = page.screenshot({
       path: hostname + ".png",
       fullPage: true,
     });
 
-    const texts = await page.$x("//text()[not(parent::script|parent::style)]");
-    const textsInnerTextPromise = texts.map((elemHandler) =>
+    const textNodes = await page.$x(
+      "//text()[not(parent::script|parent::style|parent::noscript)]"
+    );
+    const textStylePromises = textNodes.map((elemHandler) =>
       elemHandler.evaluate((elem) => {
-        style = getComputedStyle(elem.parentNode);
+        style = getComputedStyle(elem.parentNode || elem);
+
         return {
           txt: elem.wholeText,
           fontSize: style.getPropertyValue("font-size"),
@@ -28,15 +32,18 @@ const device = puppeteer.devices["Galaxy Note 3"];
         };
       })
     );
-    return Promise.all([scsPromise, ...textsInnerTextPromise]);
+    return Promise.all([scsPromise, ...textStylePromises]);
   };
 
   const results = await Promise.all(urls.map(getInfo));
   const resultsJson = results.map(([_, ...textStyle], urlIdx) => {
-    const mainContentFirst = textStyle.find((t) => t.txt.length > 100);
-    return { site: url[urlIdx], ...mainContentFirst };
+    // find second paragraph over 30 words.
+    const mainContentFirst = textStyle.filter(
+      (t) => t.txt.trim().split(" ").length > 30
+    );
+    return { site: urls[urlIdx], ...mainContentFirst[1] };
   });
   await browser.close();
 
-  await fs.writeFile("result.json", JSON.stringify(resultsJson), () => {});
+  await fs.writeFile("result.json", JSON.stringify(resultsJson));
 })();
