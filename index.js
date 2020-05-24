da58178653a91619d2fcb0566b38a3d58515d327
Jang Whemoon <palindrom615@gmail.com>
2020-05-24 11:22:53 +0000

    Fix console

--- index.js
+++ index.js
@@ -3,6 +3,49 @@ const fs = require("fs").promises;
 
 const device = puppeteer.devices["Galaxy Note 3"];
 
+const getInfo = async ([journal, url], browser) => {
+  const page = await browser.newPage();
+  await page.emulate(device);
+  page.on("console", (msg) => {
+    for (let i = 0; i < msg._args.length; ++i)
+      console.log(`${i}: ${msg._args[i]}`);
+  });
+  try {
+    await page.goto(url, {
+      waitUntil: "domcontentloaded",
+      timeout: 0,
+    });
+  } catch (e) {
+    console.log(e, journal, url);
+    return;
+  }
+
+  const scsPromise = page.screenshot({
+    path: journal + ".png",
+    fullPage: true,
+  });
+
+  const textNodes = await page.$x(
+    "//text()[not(parent::script|parent::style|parent::noscript)]"
+  );
+  const textStylePromises = textNodes.map((elemHandler) =>
+    elemHandler.evaluate((elem) => {
+      try {
+        style = getComputedStyle(elem.parentNode || elem);
+      } catch (e) {
+        console.log(e, location.href);
+        return;
+      }
+      return {
+        txt: elem.wholeText,
+        fontSize: style.getPropertyValue("font-size"),
+        fontFamily: style.getPropertyValue("font-family"),
+      };
+    })
+  );
+  return Promise.all([scsPromise, ...textStylePromises]);
+};
+
 (async () => {
   const journals = (await fs.readFile("./journals", "utf8"))
     .trim()
@@ -11,47 +54,9 @@ const device = puppeteer.devices["Galaxy Note 3"];
     .filter((arr) => arr.length === 2 && arr[1] !== "");
   const browser = await puppeteer.launch();
 
-  const getInfo = async ([journal, url]) => {
-    const page = await browser.newPage();
-    await page.emulate(device);
-    try {
-      await page.goto(url, {
-        waitUntil: "domcontentloaded",
-        timeout: 0,
-      });
-    } catch (e) {
-      console.log(e, journal, url);
-      return;
-    }
-
-    const { hostname } = new URL(url);
-    const scsPromise = page.screenshot({
-      path: journal + ".png",
-      fullPage: true,
-    });
-
-    const textNodes = await page.$x(
-      "//text()[not(parent::script|parent::style|parent::noscript)]"
-    );
-    const textStylePromises = textNodes.map((elemHandler) =>
-      elemHandler.evaluate((elem) => {
-        try {
-          style = getComputedStyle(elem.parentNode || elem);
-        } catch (e) {
-          console.log(e, journal, url);
-          return;
-        }
-        return {
-          txt: elem.wholeText,
-          fontSize: style.getPropertyValue("font-size"),
-          fontFamily: style.getPropertyValue("font-family"),
-        };
-      })
-    );
-    return Promise.all([scsPromise, ...textStylePromises]);
-  };
-
-  const results = await Promise.all(journals.map(getInfo));
+  const results = await Promise.all(
+    journals.map((journal) => getInfo(journal, browser))
+  );
   const resultsJson = results
     .filter((result) => typeof result !== "undefined")
     .map(([_, ...textStyle], journalIdx) => {
